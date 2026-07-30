2e3319990e7d0f6795d53efcf995cd4b14815990
Oddbjorn Lona <oddbjorn.lona@skuld.com>
2026-07-30 19:59:08 +0200

    GFX: Update maps and gfx

--- assets/maps/level3/temple_demo_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="temple" tilewidth="128" tileheight="128" spacing="128" margin="128" tilecount="4" columns="1">
- <image source="../gfx/tileset_desert.png" width="480" height="1200"/>
-</tileset>
